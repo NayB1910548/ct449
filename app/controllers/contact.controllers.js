742f9396dfe3de8005059d51997adef146c87154
NayB1910548 <nayb1910548@student.ctu.edu.vn>
2022-09-29 11:21:18 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controllers.js
@@ -0,0 +1,23 @@
+exports.create = (req, res) => {
+    res.send({ message: "create handler"});
+};
+
+exports.findALL = (req, res) => {
+    res.send({ message: "findALL handler"});
+};
+
+exports.findOne = (req, res) => {
+    res.sed({ message: "findOne handler"});
+};
+
+exports.update = (req, res) => {
+    res.send({ message: "update handler" });
+};
+
+exports.delete = (req, res) => {
+    res.sed({ message: "delete handler"});
+};
+
+exports.deleteAll = (req, res) => {
+    res.send({ message: "findAllFavorite handler"});
+};
